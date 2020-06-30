0f8488fbdeb784bb6cc3e94f198f05d106b37e72
sbboard <colin.buffum@gmail.com>
2020-06-29 19:25:30 -0600

    added vue router

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Home from "./components/index.vue"
+import Audio from "./components/audio.vue"
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [{
+        path: "/",
+        component: Home
+      },
+      {
+        path: "/audio/:id",
+        component: Audio
+      },
+  ]
+});
