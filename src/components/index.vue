88145716a6ec3327eba14abd1fd4a981668c73cc
sbboard <colin.buffum@gmail.com>
2020-08-07 18:34:21 -0600

    added instructions / mad tablet size

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="indexWrap">
     <h1>Last Tapes of Fate</h1>
+    <div id="boxBox">
     <div id="cautionBox">
       <h2 class="normText"><i class="fas fa-exclamation-triangle"></i> CAUTION</h2>
       <p class="normText"><b>You are about to enter the world of the auditory.</b><br>
       The only tool you will have to navigate this new landscape is your sense of sound. Your only companion, a cassette player. 
   You will be able to explore this world via the unique and curious TAPES OF FATE. 
-  Listening to one of these tapes will transport your consciousness to an actual parallel dimension. 
-  <!--<br><br>
-  At the end of each tape you will be presented with two options: a tape on the left and a tape on the right. For those using a cassette player remotely via computer technology, 
-  <b>press the F key to insert the tape on the left or press the J key to insert the tape on the right</b>. <br>
-  There may be moments during your adventure you will want to take action (such as to pick up a key, or open a door), 
-  in which case you can <b>interact with the universe recorded on the tape by pressing the G or H key.</b><br><br> -->
+  Listening to one of these tapes will transport your consciousness to an actual parallel dimension.<br>
   If you agree to proceed after hearing this warning, close your eyes to avoid dimensional interference, open your ears, and stay safe.
       </p>
     </div>
+
+<div id="instructions" class="normText">
+  <h2>Keyboard Controls</h2>
+<div>
+<span><b>Select Left Tape:</b> F</span>
+<span><b>Select Right Tape:</b> J</span>
+<i>Note: You can identity the F and J keys with your eyes closed by their notches on most keyboards.</i><br><br>
+<span><b>Action:</b> G, H <i>(keys in between F and J)</i></span>
+<span><b>Restart Tape:</b> D, K <i>(keys outside of F and J)</i></span>
+<span><b>Eject:</b> Escape</span>
+</div>
+  </div>
+  </div>
+
       <div v-for="(item, index) in localPlaylist" :key="item.name" class="albumBox">
       <router-link :to="'/'+index+'/1'"><img :alt='item.name + " cover"' :src='"/audio/"+index+"/"+item.img'/></router-link>
       <br>
@@ -62,12 +72,23 @@ export default {
 
 <style lang="sass">
 @mixin mobile
-  @media (max-width: #{700px})
+  @media (max-width: #{650px})
+      @content
+
+
+@mixin tablet
+  @media (min-width: 651px) and (max-width: 1232px)
       @content
 
 @font-face
   font-family: invisibleFont
   src: url("/fonts/New_Cicle_Fina.ttf")
+
+#boxBox
+  justify-content: space-between
+  display: flex
+  @include mobile
+    display: block
 
 footer
   position: absolute
@@ -81,8 +102,12 @@ html
   background-color: #1f1f1f
   color: white
   font-family: invisibleFont
+  @include tablet
+    background-color: red
+  @include mobile
+    background-color: green
   #indexWrap
-    max-width: 700px
+    max-width: 1200px
     display: block
     margin: 0 auto
     padding: 1em
@@ -90,6 +115,7 @@ html
       font-family: Arial, Helvetica, sans-serif
     h1
       font-size: 2em
+      text-align: center
     h2
       font-size: 1.5em
       margin-top: 1em
@@ -100,6 +126,12 @@ html
       margin-top: 1em
       padding: .5em
       border-radius: .5em
+      font-size: .75em
+      display: inline-block
+      width: calc(50% - 2em)
+      @include mobile
+        width: -webkit-fill-available
+        display: inline-block
       b
         font-weight: 600
       h2
@@ -111,6 +143,14 @@ html
         margin-bottom: .5em
         font-weight: 800
         border-radius: .25em
+    #instructions
+      @extend #cautionBox
+      h2
+        background-color: #ccc
+      span
+        display: block
+      i
+        font-style: italic
     .albumBox
       display: inline-block
       margin-top: 1em
